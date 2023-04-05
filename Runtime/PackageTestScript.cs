0599af370c5e20be16a547cd53a150a4c498f010
jeffpwang <jfywng@gmail.com>
2023-04-05 17:07:53 -0400

    test update

--- Runtime/PackageTestScript.cs
+++ Runtime/PackageTestScript.cs
@@ -7,7 +7,7 @@ public class PackageTestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("started....v3");
+        print("started....v0");
     }
 
     // Update is called once per frame
